b01be09ff16b4033958af3b69d2e97962873093a
sheikh4200 <159758352+sheikh4200@users.noreply.github.com>
2024-11-26 14:04:23 +0500

    Connect to MongoBD

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+
+dotenv.config({
+    path:"./env"
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+//using iffy method
+//using async and try and catch method
+(async()=>{
+try{
+await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+app.on("error",()=>{
+    console.log("ERR","app is not connect to db")
+    throw error
+})
+app.listen(process.env.PORT,()=>{
+console.log(`app is running on port ${process.env.PORT}`)
+})
+}
+catch(error){}
+console.log("error",error)
+})()
+*/
